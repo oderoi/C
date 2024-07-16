f36e46d245b9f6f47487f8265947c7d39495df73
io <50592711+oderoi@users.noreply.github.com>
2024-07-16 05:15:08 +0300

    Update Matrix_Multiplication.c

--- Algoritm/Math/Matrix_Multiplication.c
+++ Algoritm/Math/Matrix_Multiplication.c
@@ -1,3 +1,30 @@
+# Matrix Multiplication in C
+
+Given two matrices A of dimensions M x K and B of dimension K x N, we want to compute their dot product C = A . B, which is also known as matrix multiplication.
+
+The dot product.
+
+$C += A . B$
+
+$1D Array.$
+
+$C_{i} = \sum\limits_{i = 0}^{n} (A_{i} . B_{i}) = A_{1}B_{1} + A_{2}B_{2} + ...+ A_{n}B_{n}$
+
+$2D Array.$
+
+$C_{i , j} = \sum\limits_{k \in [0 ... K) } (A_{i , k} . B_{k , j})$
+
+$C_{i , j} = A_{i , k} . B_{k , j}$
+
+Consider
+
+$A \in \mathbb{R}^{m \times k}  ==   A \in \mathbb{R}^{rowA \times colA}$
+
+$B \in \mathbb{R}^{k \times n}  ==   B \in \mathbb{R}^{rowB \times colB}$
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
